883957eab00f88df095c60a8bb316f4c8832c4bf
Dindihub <sadindi03@gmail.com>
2022-03-28 21:54:43 +0300

    FT-Add mouseover effect on what-about-us section

--- js/script.js
+++ js/script.js
@@ -3,39 +3,52 @@ let el = document.getElementById("design-caption");
 let el2 = document.getElementById("development-caption");
 let el3 = document.getElementById("product-caption")
 
-el.addEventListener('click', (e) => {
-
-    document.getElementById('paragraph-design').style.display = "block";
-
+el.addEventListener('mouseover', (e) => {
+document.getElementById('paragraph-design').style.display = "block";
+});
+el.addEventListener('mouseout', (e) => {
+document.getElementById('paragraph-design').style.display = "";
 });
 
-el2.addEventListener('click', (e) => {
 
-    document.getElementById('para-development').style.display = "block";
-
+el2.addEventListener('mouseover', (e) => {
+document.getElementById('para-development').style.display = "block";
+});
+el2.addEventListener('mouseout', (e) => {
+document.getElementById('para-development').style.display = "";
 });
 
-el3.addEventListener('click', (e) => {
-
-    document.getElementById('para-product').style.display = "block";
-
+el3.addEventListener('mouseover', (e) => {
+document.getElementById('para-product').style.display = "block";
+});
+el3.addEventListener('mouseout', (e) => {
+document.getElementById('para-product').style.display = "";
 });
 
 // overlay on portfolio section
 let images = document.querySelectorAll(".portfolio")
 let overlay = document.querySelectorAll(".overlay")
 for (let i = 0; i <= images.length; i++) {
-    images[i].addEventListener("mouseover", (e) => {
+    images[i].addEventListener("mouseover", e => {
         overlay[i].style.display = "block"
     })
-    images[i].addEventListener("mouseout", (e) => {
-        overlay[i].style.display = "none"
+    images[i].addEventListener("mouseout", e => {
+        overlay[i].style.display = ""
     });
 }
 
 // validate form
 
-let oneForm=document.getElementById("form").addEventListener('click', e => {
-e.preventDefault();
-alert("Your form has been submitted")
+let form = document.querySelector("#form");
+    form.addEventListener("submit", e => {
+        document.getElementById("demo").innerHTML = "Your details have been submitted";
+
+        // alert("Your details have been submitted");
+    });
+
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+    
+
+    clearInputFields();
 });
